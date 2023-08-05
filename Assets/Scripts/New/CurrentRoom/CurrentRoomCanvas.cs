05e44abf837001dcd25c437d0499844a970f8888
Caddwithadot <tcadd47@gmail.com>
2023-08-04 21:47:45 -0700

    name dupes check

--- Assets/Scripts/New/CurrentRoom/CurrentRoomCanvas.cs
+++ Assets/Scripts/New/CurrentRoom/CurrentRoomCanvas.cs
@@ -1,8 +1,10 @@
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
 public class CurrentRoomCanvas : MonoBehaviour
 {
+    /*
     public void OnClickStartSync()
     {
         if (!PhotonNetwork.IsMasterClient)
@@ -20,9 +22,24 @@ public class CurrentRoomCanvas : MonoBehaviour
         PhotonNetwork.CurrentRoom.IsVisible = false;
         PhotonNetwork.LoadLevel("Game");
     }
+    */
 
     public void OnClickStart()
     {
+        foreach (Player player in PhotonNetwork.CurrentRoom.Players.Values)
+        {
+            if (player != null && player != PhotonNetwork.LocalPlayer)
+            {
+                if (player.NickName == PhotonNetwork.NickName)
+                {
+                    Debug.Log("Nickname '" + PhotonNetwork.NickName + "' is already taken.");
+                    return;
+                }
+            }
+        }
+
+        Debug.Log("Nickname '" + PhotonNetwork.NickName + "' is available.");
+
         PhotonNetwork.LoadLevel("Game");
     }
 }
