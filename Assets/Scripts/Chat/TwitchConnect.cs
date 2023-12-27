4cc05e03916f2d695014e0bdd2a7ce69f1018f0b
Caddwithadot <tcadd47@gmail.com>
2023-12-26 20:49:45 -0800

    emote stuff i started testing a while ago, gotta go in and add dictionaries for animated emotes and betterttv emotes

--- Assets/Scripts/Chat/TwitchConnect.cs
+++ Assets/Scripts/Chat/TwitchConnect.cs
@@ -1,6 +1,6 @@
 /*******************************************************************************
 Author: Taylor
-State: Complete
+State: Working, trying to figure out how to get more information from a chatter
 Description:
 Recieves all information from specified Twitch chat.
 *******************************************************************************/
@@ -10,6 +10,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Net.Sockets;
 using System.IO;
+using System;
+using UnityEngine.Networking;
+using UnityEditor.PackageManager;
+using UnityEngine.tvOS;
+using UnityEditor;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class BTTVEmoteInfo
+{
+    public string id;
+    public string code;
+    public string imageType;
+}
 
 public class TwitchConnect : MonoBehaviour
 {
@@ -19,9 +33,6 @@ public class TwitchConnect : MonoBehaviour
 
     private ChatManager chatManager;
 
-    const string URL = "irc.chat.twitch.tv";
-    const int PORT = 6667;
-
     string User = "sgp_alt";
     public string Channel;
 
@@ -29,6 +40,12 @@ public class TwitchConnect : MonoBehaviour
     private string OAuth;
 
     float PingCounter = 0;
+
+    //emote test
+    public GameObject emotePrefab;
+    private HashSet<string> uniqueEmoteIDs = new HashSet<string>();
+    public Transform emoteParent;
+
 
     private void Awake()
     {
@@ -38,13 +55,23 @@ public class TwitchConnect : MonoBehaviour
 
     private void ConnectToTwitch()
     {
-        Twitch = new TcpClient(URL, PORT);
+        Twitch = new TcpClient("irc.chat.twitch.tv", 6667);
         Reader = new StreamReader(Twitch.GetStream());
         Writer = new StreamWriter(Twitch.GetStream());
 
         Writer.WriteLine("PASS " + OAuth);
+
+        //displayname is useless, it's the same as the username, just allows the user to change capital lettering
         Writer.WriteLine("NICK " + User.ToLower());
+
+        //we want to track the username in the data sheet since their nickname can be changed at any time
+        Writer.WriteLine("USER " + User.ToLower() + " 8 * :" + User.ToLower());
+
+        //joining channel and requesting additional information
         Writer.WriteLine("JOIN #" + Channel.ToLower());
+        Writer.WriteLine("CAP REQ :twitch.tv/tags");
+        Writer.WriteLine("CAP REQ :twitch.tv/commands");
+        Writer.WriteLine("CAP REQ :twitch.tv/membership");
         Writer.Flush();
 
         print("Connected to " + Channel + " 's chat.");
@@ -53,9 +80,9 @@ public class TwitchConnect : MonoBehaviour
     void Update()
     {
         PingCounter += Time.deltaTime;
-        if(PingCounter > 60)
+        if (PingCounter > 60)
         {
-            Writer.WriteLine("PING " + URL);
+            Writer.WriteLine("PING :tmi.twitch.tv");
             Writer.Flush();
             PingCounter = 0;
         }
@@ -66,28 +93,163 @@ public class TwitchConnect : MonoBehaviour
             ConnectToTwitch();
         }
 
-        if(Twitch.Available > 0)
+
+        //UPDATE THIS STUFFS
+        if (Twitch.Available > 0)
         {
             string message = Reader.ReadLine();
 
+            //message from chatter
             if (message.Contains("PRIVMSG"))
             {
-                // :sgp_alt!sgp_alt@sgp_alt.tmi.twitch.tv PRIVMSG #possiblycadd :test test
-                int splitPoint = message.IndexOf("!");
-                string chatter = message.Substring(1, splitPoint - 1);
+                //@badge - info =; badges = broadcaster / 1; client - nonce = 22a3689b9615bd8ece35d5896e123747; color =; display - name = PossiblyCadd; emotes =; first - msg = 0; flags =; id = da670411 - cac1 - 4518 - a786 - 01d698dff049; mod = 0; returning - chatter = 0; room - id = 587142171; subscriber = 0; tmi - sent - ts = 1703275843617; turbo = 0; user - id = 587142171; user - type = :possiblycadd!possiblycadd @possiblycadd.tmi.twitch.tv PRIVMSG #possiblycadd :test
 
-                splitPoint = message.IndexOf(":", 1);
-                string msg = message.Substring(splitPoint + 1);
+                Debug.Log(message);
 
-                //sends the information to the chatManager
-                chatManager.NewMessage(chatter, msg);
+                //get the name 
+                string[] stringSeparators = new string[] { "display-name=" };
+                string[] result = message.Split(stringSeparators, StringSplitOptions.None);
+                var splitPoint = result[1].IndexOf(";", 0);
+                var chatter = result[1].Substring(0, splitPoint);
+
+                //get the users message by splitting it from the string
+                string[] stringSeparators2 = new string[] { "PRIVMSG" };
+                string[] result2 = message.Split(stringSeparators2, StringSplitOptions.None);
+                var splitPoint2 = result2[1].Split(':');
+                string msg = splitPoint2[1];
+
+                //output the username and chat message
+                chatManager.NewMessage(chatter.ToLower(), msg);
+
+
+                //EMOTE STUFFS
+                // if the emotes list is not empty, get the emote texture
+                if (!message.Contains("emotes=;"))
+                {
+                    string[] stringSeparatorsE = new string[] { "emotes=" };
+                    string[] resultE = message.Split(stringSeparatorsE, StringSplitOptions.None);
+
+                    // split the emote string in case of multiple emotes
+                    var splitPointallEmotes = resultE[1].IndexOf(";", 0);
+                    var allemotes = resultE[1].Substring(0, splitPointallEmotes);
+                    string[] seperateEmotes = allemotes.Split('/');
+
+                    // grab all emote textures
+                    for (int i = 0; i < seperateEmotes.Length; i++)
+                    {
+                        var id = seperateEmotes[i].IndexOf(":", 0);
+                        var emoteID = seperateEmotes[i].Substring(0, id);
+
+                        // 1.0 / 2.0 / 3.0 is texture sizes
+                        // light / dark is the theme mode
+                        // static / animated is if the emote is animated or not
+
+                        //global twitch
+                        //StartCoroutine(GetTexture("https://static-cdn.jtvnw.net/emoticons/v2/" + emoteID + "/static" + "/light/3.0", emoteID));
+
+                        //global betterttv
+                        StartCoroutine(GetBTTVEmoteInfo("https://cdn.betterttv.net/emote/" + emoteID + "/3x", emoteID));
+                    }
+                }
+
+                //pongs the twitch chat back to prevent us from getting kicked from the chat
+                if (message.Contains("PING :tmi.twitch.tv"))
+                {
+                    Writer.WriteLine("PONG " + "tmi.twitch.tv" + "\r\n");
+                    Writer.Flush();
+                }
             }
+        }
+    }
 
-            if(message.Contains("PING :tmi.twitch.tv"))
+    //getting the emotes texture
+    IEnumerator GetTexture(string url, string ID)
+    {
+        // find the emote texture
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+        yield return www.SendWebRequest();
+
+        // set it to an image, and spawn a particle with that image
+        Texture2D img = DownloadHandlerTexture.GetContent(www);
+
+        //emote particle test
+        GameObject emotePart = Instantiate(emotePrefab, transform.position, transform.rotation);
+        emotePart.GetComponent<ParticleSystem>().GetComponent<Renderer>().material.mainTexture = img;
+
+        //GameObject emotePart = Instantiate(emotePrefab, transform.position, transform.rotation, emoteParent);
+        //emotePart.GetComponent<Image>().material.mainTexture = img;
+
+        // save the texture to a folder
+        //SaveTextureToFile(img, ID);
+    }
+
+    // Fetch BetterTTV emote information
+    IEnumerator GetBTTVEmoteInfo(string url, string emoteID)
+    {
+        UnityWebRequest infoRequest = UnityWebRequest.Get(url);
+        yield return infoRequest.SendWebRequest();
+
+        if (infoRequest.isNetworkError || infoRequest.isHttpError)
+        {
+            Debug.LogError($"Error fetching BetterTTV emote information for {emoteID}: {infoRequest.error}");
+        }
+        else
+        {
+            BTTVEmoteInfo emoteInfo = JsonUtility.FromJson<BTTVEmoteInfo>(infoRequest.downloadHandler.text);
+
+            if (emoteInfo != null)
             {
-                Writer.WriteLine("PONG " + "tmi.twitch.tv" + "\r\n");
-                Writer.Flush();
+                // Check if the BetterTTV emote is a GIF
+                if (emoteInfo.imageType == "gif")
+                {
+                    Debug.Log("Emote was a gif");
+                }
+                else
+                {
+                    // Fetch the BetterTTV emote PNG texture
+                    StartCoroutine(GetBTTVEmoteTexture($"{url}/3x"));
+                }
             }
+            else
+            {
+                Debug.LogError($"Failed to parse BetterTTV emote information for {emoteID}");
+            }
+        }
+    }
+
+    // Display BetterTTV emote PNG texture
+    IEnumerator GetBTTVEmoteTexture(string url)
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+        yield return www.SendWebRequest();
+
+        Texture2D img = DownloadHandlerTexture.GetContent(www);
+
+        // Instantiate emote particle with the texture
+        GameObject emotePart = Instantiate(emotePrefab, transform.position, transform.rotation);
+        emotePart.GetComponent<ParticleSystem>().GetComponent<Renderer>().material.mainTexture = img;
+    }
+
+    //saves texture to the EmoteTextures folder
+    void SaveTextureToFile(Texture2D texture, string emoteID)
+    {
+        byte[] bytes = texture.EncodeToPNG();
+
+        // Create a folder named "EmoteTextures" within the Assets folder
+        string folderPath = Application.dataPath + "/EmoteTextures";
+        if (!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+        }
+
+        // Check if the emote ID is already saved
+        if (!uniqueEmoteIDs.Contains(emoteID))
+        {
+            // Save the texture to a file in the folder
+            File.WriteAllBytes(folderPath + "/" + emoteID + ".png", bytes);
+
+            // Add the emote ID to the set of unique emote IDs
+            uniqueEmoteIDs.Add(emoteID);
         }
     }
 }
